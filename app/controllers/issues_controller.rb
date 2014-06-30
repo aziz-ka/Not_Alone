5c4f8b5b60fb9c2655b197b661125cced4d5a41d
aziz-ka <hasanov.aziz@gmail.com>
2014-06-29 21:18:40 -0400

    removed twilio credentials

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -7,8 +7,6 @@ class IssuesController < ApplicationController
   def index
     # binding.pry
 
-    # account_sid = "AC877d12a6bed8d852b0e3e70ff65cf3e7"
-    # auth_token = "863d16e53b9c49c90a53885af77af078"
     # @client = Twilio::REST::Client.new account_sid, auth_token
     @seniors = Senior.all
     @issues = Issue.all
