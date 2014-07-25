13e98e9096359afd764ad243ce13be4c063aeca2
aziz-ka <hasanov.aziz@gmail.com>
2014-07-25 15:38:47 -0400

    tweaked issues index view and fixed bug in twilio controller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -5,19 +5,21 @@ class TwilioController < ApplicationController
     # process the number of the sender
     number = params["From"][2..11]
 
-    # Find the senior that has sent the text
-    if Senior.find_by(phone_number: number) != nil
-      senior = Senior.find_by(phone_number: number)
-    end
-
     # Get the content of the message
     content = params["Body"]
 
-    # Create new issue
-    issue = Issue.create(content: content)
+    if Senior.exists?(phone_number: number)
 
-    # Associate senior with new issue
-    senior.issues << issue
+      # Create new issue
+      issue = Issue.create(content: content)
+
+      # Find the senior that has sent the text
+      senior = Senior.find_by(phone_number: number)
+
+      # Associate senior with new issue
+      senior.issues << issue
+
+    end
 
   end
 
